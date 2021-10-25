19411d61d6c00e16a4bc31864ffaf5e822048dff
Use your GitHub username <Enter your email address>
2021-10-25 15:44:40 -0600

    lesson6

--- lesson6/js/windchill.js
+++ lesson6/js/windchill.js
@@ -3,10 +3,14 @@ function windChill() {
     let windSpeed = document.getElementById("wind-speed");
     let highTemp = document.getElementById("high");
 
-    if (windSpeed > 3 && highTemp < 50) {
+    windSpeed == 8;
+    highTemp == 61;
+    windChill == "NA";
+
+    if (windSpeed > 3 && highTemp <= 50) {
         windChill = 35.74 + 0.6215 * highTemp - 35.75 * windSpeed ** 0.16 + 0.4275 * windspeed * highTemp ** 0.16;
     }
     else {
-        windChill = "N/A";
+        windChill = "NA";
     }
 }
