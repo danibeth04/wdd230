11d1d21d5b4857ccff3021e4cfc97ffbc5ddd336
Use your GitHub username <Enter your email address>
2021-11-11 16:00:35 -0700

    lesson9

--- lesson6/js/windchill.js
+++ lesson6/js/windchill.js
@@ -5,7 +5,6 @@ function windChill() {
 
     windSpeed == 8;
     highTemp == 61;
-    windChill == "NA";
 
     if (windSpeed > 3 && highTemp <= 50) {
         windChill = 35.74 + 0.6215 * highTemp - 35.75 * windSpeed ** 0.16 + 0.4275 * windspeed * highTemp ** 0.16;
--- lesson9/js/prophets.js
+++ lesson9/js/prophets.js
@@ -1,29 +1,30 @@
+// Store the URL of the JSON file
 const requestURL = 'https://byui-cit230.github.io/lessons/lesson-09/data/latter-day-prophets.json';
 
 fetch(requestURL)
-  .then(function (response) {
-    return response.json();
-  })
-  .then(function (jsonObject) {
-    console.table(jsonObject);  // temporary checking for valid response and data parsing
-    const prophets = jsonObject['prophets'];
-    for (let i = 0; i < prophets.length; i++ ) {
+    .then(function (response) {
+        return response.json();
+    })
+    .then(function (jsonObject) {
+        // console.table(jsonObject); // temporary checking for valid response and data parsing
+        const prophets = jsonObject['prophets']
+        prophets.forEach(prophet => {
+            let card = document.createElement('section');
+            let h2 = document.createElement('h2');
+            let birthDate = document.createElement('p');
+            let birthPlace = document.createElement('p');
+            let img = document.createElement('img');
 
-        let card = document.createElement('section');
-        let h2 = document.createElement('h2');
-        let birthDate = document.createElement('p');
-        let birthPlace = document.createElement('p')
+            h2.textContent = `${prophet.name} ${prophet.lastname}`
+            birthDate.textContent = `Date of Birth: ${prophet.birthdate}`
+            birthPlace.textContent = `Place of Birth: ${prophet.birthplace}`
+            img.setAttribute('src', prophet.imageurl)
+            img.setAttribute('alt', `${prophet.name} ${prophet.lastname} - ${prophet.order}`)
 
-        h2.textContent = prophets[i].name + ' ' + prophets[i].lastname;
-        birthDate.textContent = 'Date of Birth: ${prophet.birthdate}'
-        birthPlace.textContent = 'Place of Birth: ${prophet.birthplace}'
-        image.setAttribute('src', prophets[i].imageurl);
-        img.setAttribute('alt', prophets[i].name + ' ' + prophets[i].lastname);
-
-        card.appendChild(h2)
-        card.appendChild(birthDate)
-        card.appendChild(birthPlace)
-        card.appendChild(img)
-        
-        document.querySelector('div.cards').appendChild(card);
-});
+            card.appendChild(h2)
+            card.appendChild(birthDate)
+            card.appendChild(birthPlace)
+            card.appendChild(img)
+            document.querySelector('div.cards').appendChild(card)
+        });
+    })
