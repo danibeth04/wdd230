1b276a427f1fda5689f07004f8885fae89d73472
Use your GitHub username <Enter your email address>
2022-10-04 11:27:25 -0600

    lesson4

--- chamber/scripts/date.js
+++ chamber/scripts/date.js
@@ -3,13 +3,19 @@ const date = new Date();
 const year = date.getFullYear();
 yearOutput.innerText = year;
 
-const lastUpdate = document.querySelector("#lastUpdate");
+//create today's date for nav header
+//en-US uses month-day-year order
+//en-UK uses day-month-year order
+const today = document.querySelector("#today");
 const options = {
     weekday: "long",
     day: "numeric",
     month: "long",
     year: "numeric"
 };
-    lastUpdate.textContent = new Date().toLocaleDateString("en-US", options);
-//en-US uses month-day-year order
-//en-UK uses day-month-year order
+today.textContent = date.toLocaleDateString("en-UK", options);
+
+//create last-updated date for footer
+//look at code for formatting date from https://bobbyhadz.com/blog/javascript-format-date-mm-dd-yyyy-hh-mm-ss
+const lastUpdate = document.querySelector("#lastUpdate");
+lastUpdate.innerHTML = date.toLocaleDateString("en-UK", options);
